7969c05fc03c0438b1276cb820e8f0c8f8e99fa3
allen <allen@dreamIt.com>
2017-04-05 23:46:20 +0800

    done

--- src/main/java/com/dram/spring/io/kafka/PlainKafkaUtil.java
+++ src/main/java/com/dram/spring/io/kafka/PlainKafkaUtil.java
@@ -1,16 +1,22 @@
 package com.dram.spring.io.kafka;
 
 import ch.qos.logback.core.util.TimeUtil;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.IntegerDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.listener.KafkaMessageListenerContainer;
 import org.springframework.kafka.listener.MessageListener;
 import org.springframework.kafka.listener.config.ContainerProperties;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
@@ -29,7 +35,7 @@ public class PlainKafkaUtil {
         ContainerProperties containerProperties = new ContainerProperties("topic1","topic2");
         containerProperties.setMessageListener(new MessageListener<Integer,String>() {
             public void onMessage(ConsumerRecord<Integer, String> data) {
-                logger.warn("==receive mesage==>" + data);
+                logger.error("\n\n\n\n==receive mesage==>{}\n\n\n" , data);
                 latch.countDown();
             }
         });
@@ -54,7 +60,7 @@ public class PlainKafkaUtil {
         producerTemplate.flush();
 
         //
-        latch.await(60, TimeUnit.SECONDS);
+        latch.await(2, TimeUnit.SECONDS);
 
         container.stop();
 
@@ -71,18 +77,32 @@ public class PlainKafkaUtil {
     }
 
     private Map<String,Object> getConsumerProperties(){
-
-        return null;
+        Map<String,Object> properties = new HashMap<>();
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,"c6:9092");
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, "group01");
+        properties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
+        properties.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG,100);
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, IntegerDeserializer.class);
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        return properties;
     }
 
     private Map<String,Object> getProducerProperties(){
-
-        return null;
+        Map<String,Object> properties = new HashMap<>();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"c6:9092");
+        properties.put(ProducerConfig.RETRIES_CONFIG, 0);
+        properties.put(ProducerConfig.BATCH_SIZE_CONFIG,16384);
+        properties.put(ProducerConfig.LINGER_MS_CONFIG,1);
+        properties.put(ProducerConfig.BUFFER_MEMORY_CONFIG,33554432);
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,IntegerDeserializer.class);
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,StringDeserializer.class);
+        return properties;
     }
 
     private KafkaTemplate<Integer,String>  createTemplate(){
 
-        DefaultKafkaProducerFactory producerFactory = new DefaultKafkaProducerFactory(getProducerProperties());
+
+        ProducerFactory producerFactory = new DefaultKafkaProducerFactory(getProducerProperties());
 
         KafkaTemplate<Integer,String> template = new KafkaTemplate<Integer, String>(producerFactory);
 
@@ -90,8 +110,9 @@ public class PlainKafkaUtil {
     }
 
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws Exception {
+        PlainKafkaUtil p = new PlainKafkaUtil();
+        p.simpleDemo4ProduceAndConume();
 
     }
 
